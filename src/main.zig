87de419a17d751fb7d74de7826269d0209f889c5
Caleb Barger <bargercj20@hotmail.com>
2023-10-30 08:46:45 -0700

    implement a few more instructions

--- src/main.zig
+++ src/main.zig
@@ -3,12 +3,24 @@
 //!
 //! Caleb Barger
 //! 10/27/23
-//! Linux x86_64
 //!
 //! Simulator for the atmega328
 //!
 
+// NOTE(caleb): questions.
+// =============================
+// - General registers are "mapped" into dmem starting at addr 0?
+// - When does the instruction processing cycle by convention terminate?
+// - Cycles? time???
+// - What is ment exactly by write back. Does that include SREG updates?
+// - Do sreg fields i.e. xor that depend on other sreg fields compute before or after sreg writes.
+
+// TODO(caleb):
+// - Finish SREG operations
+// - Conventions for decoding sections of instructions, naming, impl, org...
+
 const std = @import("std");
+const elf = std.elf;
 
 const SReg = packed struct {
     /// Carry flag
@@ -30,8 +42,17 @@ const SReg = packed struct {
 };
 
 const Instruction = enum {
+    // ALU instructions
     add,
     nop,
+    cp,
+    adc,
+    @"and",
+    eor,
+    @"or",
+    mov,
+
+    // Immediate instructions
     ldi,
 };
 
@@ -43,6 +64,7 @@ const stack_end: u16 = 0x08ff;
 
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    var scratch_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     var ally = arena.allocator();
 
     ////////////////////
@@ -102,58 +124,168 @@ pub fn main() !void {
     ////////////////////
 
     // Program counter
-    var pc: u16 = 0;
+    var pc: u14 = 0;
 
     //        Program memory
     // -----------------------------
-    // | Application flash section | 0x0000
+    // | Application flash section | 0x0000-0x37ff
     // -----------------------------
-    // |    Boot flash section     | 0x3FFF
+    // |    Boot flash section     | 0x3800-0x3fff
     // -----------------------------
     var pmem = try ally.alloc(u16, 0x4000);
     for (pmem) |*byte| byte.* = 0;
 
     ////////////////////
 
-    // Process instructions!!!
+    // Load instructions into pmem
+    {
+        const restore_state = scratch_arena.state;
+        defer scratch_arena.state = restore_state;
 
-    pmem[0] = 0xe001; // ldi r16, 1
-    pmem[1] = 0xe012; // ldi r16, 2
+        var args_iter = std.process.args();
+        _ = args_iter.skip();
+        const elf_path = args_iter.next() orelse unreachable;
 
-    while (pmem[pc] != 0) {
-        // 1) Fetch instruction from memory
-        const op1: u4 = @intCast(pmem[pc] >> 12);
+        var elf_file = try std.fs.cwd().openFileZ(elf_path, .{});
+        defer elf_file.close();
 
-        // 2) Decode instructions
-        var instruction: Instruction = undefined;
-        switch (op1) {
-            0x0 => {
-                // add, nop
-                const op2: u2 = @truncate(pmem[pc] >> 10);
-                switch (op2) {
-                    0x3 => instruction = .add,
-                    0x0 => instruction = .nop,
-                    else => unreachable,
+        var elf_bytes = try elf_file.readToEndAlloc(scratch_arena.allocator(), 1024 * 10);
+
+        const elf_header = try elf.Header.read(elf_file);
+
+        // HACK(caleb): Write insturctions to pmem after finding the first
+        // loadable program header with an executable flag.
+        var program_header_iter = elf_header.program_header_iterator(elf_file);
+        while (try program_header_iter.next()) |p_header| {
+            if ((p_header.p_type == elf.PT_LOAD) and
+                ((p_header.p_flags & elf.PF_X) != 0))
+            {
+                var pmem_index: usize = 0;
+                var byte_index: usize = 0;
+                while (byte_index < p_header.p_memsz) : (byte_index += 2) {
+                    const first_byte_index = p_header.p_offset + byte_index;
+                    pmem[pmem_index] = std.mem.readIntSlice(
+                        u16,
+                        elf_bytes[first_byte_index .. first_byte_index + 2],
+                        .Little,
+                    );
+                    pmem_index += 1;
                 }
-            },
-            0x1 => {
-                // cp, adc
-            },
-            0x2 => {
-                // and, eor, or, mov
-            },
-            0xe => instruction = .ldi,
-            else => unreachable,
+                break; // Instructions loaded. Move on.
+            }
         }
+    }
+
+    ////////////////////
+
+    // Instruction processing loop
+
+    while (pc < 0x3800) { //pmem[pc] != 0) { // FIXME(caleb): How is this normally done?
+        // NOTE(caleb): Most instructions don't modify the pc so just increment
+        // unless told otherwise.
+        var next_pc_value = pc + 1;
+
+        // 1) Fetch instruction from memory NOTE(caleb): the implicit pmem[pc]
+        // 2) Decode instructions
+
+        const op1: u4 = @truncate(pmem[pc] >> 12);
+        const op2: u2 = @truncate(pmem[pc] >> 10);
+
+        // var instruction: Instruction = undefined;
+        var instruction: Instruction = switch (op1) {
+            // ALU instructions
+            //  15:12 11:10 9:8   7:4    3:0
+            // --------------------------------
+            // | op1 | op2 | rd | dddd | rrrr |
+            // --------------------------------
+            0x0 => switch (op2) { // add, nop
+                0x3 => .add,
+                0x0 => .nop,
+                else => unreachable,
+            },
+            0x1 => switch (op2) { // cp, adc
+                0x1 => .cp,
+                0x3 => .adc,
+                else => unreachable,
+            },
+            0x2 => switch (op2) { // and, eor, or, mov
+                0x0 => .@"and",
+                0x1 => .eor,
+                0x2 => .@"or",
+                0x3 => .mov,
+            },
+
+            // Immediate instructions
+            //  15:12  11:8   7:4    3:0
+            // ----------------------------
+            // | op1 | KKKK | dddd | KKKK |
+            // ----------------------------
+            0xe => .ldi,
+            else => unreachable,
+        };
 
         switch (instruction) {
-            .add => unreachable,
+            .add => {
+                // Source/Dest registers
+                const source_regh: u8 = @truncate((pmem[pc] >> 5) & (1 << 4));
+                const source_regl: u8 = @truncate(pmem[pc] & 0x0f);
+                const dest_reg: u5 = @truncate(pmem[pc] >> 4);
+
+                // Do the add
+                const result = @addWithOverflow(gp_regs[dest_reg], gp_regs[source_regh | source_regl]);
+                gp_regs[dest_reg] = result[0];
+
+                // Update SREG
+                sreg.c = result[1];
+                sreg.n = @intCast(gp_regs[dest_reg] >> 7);
+                sreg.z = @intFromBool(gp_regs[dest_reg] == 0);
+            },
+            .nop => break, // No operation..
+            .cp => {
+                // Source/Dest registers
+                const source_regh: u8 = @truncate((pmem[pc] >> 5) & (1 << 4));
+                const source_regl: u8 = @truncate(pmem[pc] & 0x0f);
+                const dest_reg: u5 = @truncate(pmem[pc] >> 4);
+
+                // Do the add
+                const result = @subWithOverflow(gp_regs[dest_reg], gp_regs[source_regh | source_regl]);
+                gp_regs[dest_reg] = result[0];
+
+                // Update SREG
+                sreg.n = @intCast(gp_regs[dest_reg] >> 7);
+                sreg.z = @intFromBool(gp_regs[dest_reg] == 0);
+            },
+            .adc => {
+                // Source/Dest registers
+                const source_regh: u8 = @truncate((pmem[pc] >> 5) & (1 << 4));
+                const source_regl: u8 = @truncate(pmem[pc] & 0x0f);
+                const dest_reg: u5 = @truncate(pmem[pc] >> 4);
+
+                // Do the add
+                const result = @addWithOverflow(gp_regs[dest_reg], @addWithOverflow(gp_regs[source_regh | source_regl], sreg.c)[0]);
+                gp_regs[dest_reg] = result[0];
+
+                // Update SREG
+                sreg.c = result[1];
+                sreg.n = @intCast(gp_regs[dest_reg] >> 7);
+                sreg.z = @intFromBool(gp_regs[dest_reg] == 0);
+            },
+            .@"and" => {
+                // Source/Dest registers
+                const source_regh: u8 = @truncate((pmem[pc] >> 5) & (1 << 4));
+                const source_regl: u8 = @truncate(pmem[pc] & 0x0f);
+                const dest_reg: u5 = @truncate(pmem[pc] >> 4);
+
+                // Logical and
+                gp_regs[dest_reg] = gp_regs[dest_reg] & gp_regs[source_regh | source_regl];
+
+                sreg.v = 0;
+                sreg.n = @intCast(gp_regs[dest_reg] >> 7);
+                sreg.z = @intFromBool(gp_regs[dest_reg] == 0);
+                sreg.s = sreg.n ^ sreg.v;
+            },
+
             .ldi => {
-                // 3) Calculate effective address of operand
-                // NOTE(caleb): Nothing in this case since data is constant
-
-                // 4) Fetch operands from memory
-
                 // Constant data
                 const cdh: u8 = @intCast((pmem[pc] >> 4) & 0x00f0);
                 const cdl: u8 = @intCast(pmem[pc] & 0x000f);
@@ -161,16 +293,17 @@ pub fn main() !void {
                 // Dest register
                 const dest_reg_index: u16 = ((pmem[pc] >> 4) & 0x000f) + 0x10;
 
-                // 5) Execute operation
+                // Execute operation
                 gp_regs[dest_reg_index] = cdh | cdl;
 
-                // 6) Store the result
+                // Store the result
                 // NOTE(caleb): Normally write back to sram here??
+                // Things like update SREG??
             },
-            .nop => unreachable,
+            else => break, // Not implemented or "bad" instruction
         }
 
-        pc += 1; // Increment program counter TODO(caleb): On a per instruction basis
+        pc = next_pc_value; // Update program counter
     }
 
     ////////////////////
